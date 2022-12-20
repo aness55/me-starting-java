97b9f596417bbb74e2be467734803f11c832f696
Anes <prentic.anes@gmail.com>
2022-12-20 23:52:24 +0100

    Added java classes

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,17 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private Integer age;
+    private LocalDate  dob;
+    private String email;
+
+    public Student() {
+
+    }
+
+    
+}
