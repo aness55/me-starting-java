23084a8b9b475894e3482ffb7a033136b1894043
Anes <prentic.anes@gmail.com>
2022-12-22 11:59:57 +0100

    Added some classes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,20 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World", "How");
 	}
 
 }
